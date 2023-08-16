580313a93341cec277a90555769f27675e2911bb
jeysonb <jeysonb@gmail.com>
2023-08-16 00:59:49 -0300

    Add files via upload

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,25 @@
+import React from 'react'
 import './style.css'
+import logo from "../assets/logo.png"
 
 export function NavBar() {
-     <div className="container">
-          <p className='item-menu'>Início</p>
-     </div>
+     return (
+          <div className="container">
+               <img src={logo} alt="Logo Los Borrachos" className='nav-logo'/>
+               <div>
+                    <ul>
+                         <li className='item-menu'><a href=''>Sobre</a></li>
+                         <li className='item-menu'><a href=''>Tintos</a></li>
+                         <li className='item-menu'><a href=''>Brancos</a></li>
+                         <li className='item-menu'><a href=''>Rosês</a></li>
+                         <li className='item-menu'><a href=''>Espumantes</a></li>
+                         <li className='item-menu btn-login'><a href=''>LOGIN</a></li>
+                         
+                    </ul>
+               </div>
+          </div>
+
+     )
 }
+
+export default NavBar;
